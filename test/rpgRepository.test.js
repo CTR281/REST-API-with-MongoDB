3ccf7ef8ad03ba08038be9daf1ddfa774ca33143
Maxime <maxime.schlegel.83@gmail.com>
2019-11-28 16:16:30 +0100

    Change tests

--- test/rpgRepository.test.js
+++ test/rpgRepository.test.js
@@ -6,12 +6,12 @@ const dbUtils = require('./utils');
 
 
 // delete all document in the rpg collection
-function clearRPGCollection() {
+function clearTestsCollection() {
     const client = new mongoClient(dbUtils.getURL());
     client.connect(function(err) {
         if (err) throw err;
         const targetDb = client.db(config.get('database.name'));
-        const targetCollection = targetDb.collection("rpg");
+        const targetCollection = targetDb.collection("tests");
         targetCollection.deleteMany(new Document());
         client.close();
     })
@@ -19,13 +19,13 @@ function clearRPGCollection() {
 
 
 // tests pre and post process
-beforeEach(() => { clearRPGCollection() });
+beforeEach(() => { clearTestsCollection() });
 
-afterEach(() => { clearRPGCollection() });
+afterEach(() => { clearTestsCollection() });
 
 
 //tests
-describe('insertDocument & findAllDocument', () => {
+describe('insertDocument & findAllDocuments', () => {
     test("empty database", () => {
         const client = new mongoClient(dbUtils.getURL());
         client.connect(function(err) {
@@ -33,8 +33,8 @@ describe('insertDocument & findAllDocument', () => {
 
             const db = client.db(config.get('database.name'));
 
-            rpgRepository.findAllDocuments(db,function(docs) {
-                expect(docs.size).toEqual(0);
+            rpgRepository.findAllDocuments(db,"tests", function(result) {
+                expect(result.docs.length).toEqual(0);
             });
 
             client.close();
@@ -48,17 +48,17 @@ describe('insertDocument & findAllDocument', () => {
 
             const db = client.db(config.get('database.name'));
 
-            rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Gandalf|75|Grey Wizard"), function(result) {
-                    expect(result).toEqual("Inserted documents into the collection");
+            rpgRepository.insertDocuments(db, "tests", dbUtils.parseCharacter("Gandalf|75|Grey Wizard"), function(result) {
+                    expect(result.message).toEqual("Inserted documents into the collection");
             });
             rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Radagast|75|Brown Wizard"), function(result) {
-                    expect(result).toEqual("Inserted documents into the collection");
+                    expect(result.message).toEqual("Inserted documents into the collection");
             });
 
-            rpgRepository.findAllDocuments(db,function(docs) {
-                expect(docs.size).toEqual(2);
-                expect(docs[0]).toEqual(dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
-                expect(docs[1]).toEqual(dbUtils.parseCharacter("Radagast|75|Brown Wizard"));
+            rpgRepository.findAllDocuments(db,function(result) {
+                expect(result.docs.length).toEqual(2);
+                expect(result.docs[0]).toEqual(dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
+                expect(result.docs[1]).toEqual(dbUtils.parseCharacter("Radagast|75|Brown Wizard"));
             });
 
             client.close();
@@ -67,6 +67,77 @@ describe('insertDocument & findAllDocument', () => {
 });
 
 describe("findDocument", () => {
+    test("target document doesn't exist", () => {
+        const client = new mongoClient(dbUtils.getURL());
+        client.connect(function(err) {
+            if (err) throw err;
+
+            const db = client.db(config.get('database.name'));
+
+            rpgRepository.findDocument(db, "test", "mydoc", function (result) {
+                expect(result.error).toBeNull();
+                expect(result.docs.length).toEqual(0);
+            });
+
+            client.close();
+        });
+    });
+
+    test("normal use", () => {
+        const client = new mongoClient(dbUtils.getURL());
+        client.connect(function(err) {
+            if (err) throw err;
+
+            const db = client.db(config.get('database.name'));
+
+            rpgRepository.insertDocuments(db, "tests", dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
+            rpgRepository.insertDocuments(db, "tests", dbUtils.parseCharacter("Radagast|75|Brown Wizard"));
+
+            rpgRepository.findAllDocuments(db,"tests", function(result) {
+                rpgRepository.findDocument(db, result.docs[0].id, function (char) {
+                    expect(char.docs.length).toEqual(1);
+                    expect(char.docs[0]).toEqual(dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
+                });
+                rpgRepository.findDocument(db, result.docs[1].id, function (char) {
+                    expect(char.docs.size).toEqual(1);
+                    expect(char.docs[1]).toEqual(dbUtils.parseCharacter("Radagast|75|Brown Wizard"));
+                });
+            });
+
+            client.close();
+        });
+    });
+});
+
+describe("updateDocument", () => {
+    test("normal use", () => {
+        const client = new mongoClient(dbUtils.getURL());
+        client.connect(function(err) {
+            if (err) throw err;
+
+            const db = client.db(config.get('database.name'));
+
+            rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
+
+            rpgRepository.findAllDocuments(db,function(result) {
+                rpgRepository.updateDocument(db, result.docs[0],
+                    dbUtils.parseCharacter("Gandalf|100|White Wizard"),
+                    function (result) {
+                    expect(result.message).toEqual("Updated the document with id:"+docs[0]);
+                });
+            });
+
+            rpgRepository.findAllDocuments(db,function(result) {
+                expect(result.docs.length).toEqual(1);
+                expect(result.docs[0]).toEqual(dbUtils.parseCharacter("Gandalf|100|White Wizard"));
+            });
+
+            client.close();
+        });
+    });
+});
+
+describe("removeDocument", () => {
     test("document doesn't exist", () => {
         const client = new mongoClient(dbUtils.getURL());
         client.connect(function(err) {
@@ -74,8 +145,11 @@ describe("findDocument", () => {
 
             const db = client.db(config.get('database.name'));
 
-            rpgRepository.findDocument(db, "test", function (err, docs) {
-                expect(err).toBeNull();
+            rpgRepository.removeDocument(db, "test", "test",function(result) {
+                expect(result.message).toEqual("Removed the document with the id:"+"test");
+            });
+
+            rpgRepository.findAllDocuments(db,function(docs) {
                 expect(docs.size).toEqual(0);
             });
 
@@ -90,107 +164,16 @@ describe("findDocument", () => {
 
             const db = client.db(config.get('database.name'));
 
-            rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
-            rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Radagast|75|Brown Wizard"));
+            rpgRepository.insertDocuments(db, "tests", dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
 
-            rpgRepository.findAllDocuments(db,function(docs) {
-                rpgRepository.findDocument(db, docs[0].id, function (docs) {
-                        expect(docs.size).toEqual(1);
-                        expect(docs[0]).toEqual(dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
-                });
-                rpgRepository.findDocument(db, docs[1].id, function (docs) {
-                    expect(docs.size).toEqual(1);
-                    expect(docs[1]).toEqual(dbUtils.parseCharacter("Radagast|75|Brown Wizard"));
+            rpgRepository.findAllDocuments(db,function(result) {
+                rpgRepository.removeDocument(db, result.docs[0],function (char) {
+                    expect(char.message).toEqual("Removed the document with the id:"+result.docs[0]);
                 });
             });
 
-            client.close();
-        });
-    });
-});
-
-describe("updateDocument", () => {
-    test("document doesn't exist", () => {
-        const client = new mongoClient(dbUtils.getURL());
-        client.connect(function(err) {
-            if (err) throw err;
-
-            const db = client.db(config.get('database.name'));
-
-            rpgRepository.updateDocument(db, "test", dbUtils.parseCharacter("Gandalf|100|White Wizard"),function (result) {
-                expect(result).toBeNull("Updated the document with id:"+"test");
-            });
-
-            rpgRepository.findAllDocuments(db,function(docs) {
-                expect(docs.size).toEqual(0);
-            });
-
-            client.close();
-        });
-    });
-
-    test("normal usage", () => {
-        const client = new mongoClient(dbUtils.getURL());
-        client.connect(function(err) {
-            if (err) throw err;
-
-            const db = client.db(config.get('database.name'));
-
-            rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
-
-            rpgRepository.findAllDocuments(db,function(docs) {
-                rpgRepository.updateDocument(db, docs[0], dbUtils.parseCharacter("Gandalf|100|White Wizard"),function (result) {
-                    expect(result).toEqual("Updated the document with id:"+docs[0]);
-                });
-            });
-
-            rpgRepository.findAllDocuments(db,function(docs) {
-                expect(docs.size).toEqual(1);
-                expect(docs[0]).toEqual(dbUtils.parseCharacter("Gandalf|100|White Wizard"));
-            });
-
-            client.close();
-        });
-    });
-});
-
-describe("deleteDocument", () => {
-    test("document doesn't exist", () => {
-        const client = new mongoClient(dbUtils.getURL());
-        client.connect(function(err) {
-            if (err) throw err;
-
-            const db = client.db(config.get('database.name'));
-
-            rpgRepository.removeDocument(db, "test", function(result) {
-                expect(result).toEqual("Removed the document with the id:"+"test");
-            });
-
-            rpgRepository.findAllDocuments(db,function(docs) {
-                expect(docs.size).toEqual(0);
-            });
-
-            client.close();
-        });
-    });
-
-    test("normal useage", () => {
-        const client = new mongoClient(dbUtils.getURL());
-        client.connect(function(err) {
-            if (err) throw err;
-
-            const db = client.db(config.get('database.name'));
-
-            rpgRepository.insertDocuments(db, dbUtils.parseCharacter("Gandalf|75|Grey Wizard"));
-
-            rpgRepository.findAllDocuments(db,function(docs) {
-                rpgRepository.removeDocument(db, docs[0],function (result) {
-                    expect(result).toEqual("Removed the document with the id:"+docs[0]);
-                });
-            });
-
-            rpgRepository.findAllDocuments(db,function(docs) {
-                expect(docs.size).toEqual(0);
+            rpgRepository.findAllDocuments(db,function(result) {
+                expect(result.docs.length).toEqual(0);
             });
 
             client.close();
