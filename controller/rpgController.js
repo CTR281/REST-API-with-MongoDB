41a6796cc3d70944eeaf91fa585ebe0959efb600
christian <chmartin2.81@gmail.com>
2019-11-28 10:01:38 +0100

    simplified imports

--- controller/rpgController.js
+++ controller/rpgController.js
@@ -3,13 +3,9 @@ const assert = require('assert');
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 
-const db_host = config.get('database.host');
-const db_port = config.get('database.port');
-const db_user = config.get("database.user");
-const db_name = config.get("database.name");
-const db_password = config.get("database.password");
+const DB = config.get('database');
 
-const db_url = `mongodb+srv://${db_user}:${db_password}@${db_host}` + (db_port ? `:${db_port}` : "") + "/";
+const db_url = `mongodb+srv://${DB.user}:${DB.password}@${DB.host}` + (DB.port ? `:${DB.port}` : "") + "/";
 
 const rpgRepository = require('../repository/rpgRepository');
 
@@ -21,7 +17,7 @@ class rpgController{
             assert.equal(null, err);
             console.log("Connected successfully to server");
 
-            const db = client.db(db_name);
+            const db = client.db(DB.name);
             const id = req.params.id;
             if (id.length === 12 || 24){
 
@@ -40,7 +36,7 @@ class rpgController{
             assert.equal(null, err);
             console.log("Connected successfully to server");
 
-            const db = client.db(db_name);
+            const db = client.db(DB.name);
 
             rpgRepository.findAllDocuments(db, function(docs) {
                 res.json(docs);
@@ -55,9 +51,10 @@ class rpgController{
             assert.equal(null, err);
             console.log("Connected successfully to server");
 
-            const db = client.db(db_name);
+            const db = client.db(DB.name);
+            const document = req.body.toArray();
 
-            rpgRepository.insertDocuments(db, [req.body], function(result) {
+            rpgRepository.insertDocuments(db, document, function(result) {
                 res.send(result);
                 client.close();
             });
@@ -70,7 +67,7 @@ class rpgController{
             assert.equal(null, err);
             console.log("Connected successfully to server");
 
-            const db = client.db(db_name);
+            const db = client.db(DB.name);
 
             rpgRepository.updateDocument(db, req.params.id, req.body, function(result) {
                 res.send(result);
@@ -85,7 +82,7 @@ class rpgController{
             assert.equal(null, err);
             console.log("Connected successfully to server");
 
-            const db = client.db(db_name);
+            const db = client.db(DB.name);
 
             rpgRepository.removeDocument(db, req.params.id, function(result) {
                 res.send(result);
